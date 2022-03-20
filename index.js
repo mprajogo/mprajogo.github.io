dbdcb0f3ed0a46b2048c858eb0a818218855353c
Michelle Prajogo <mprajogo@gmail.com>
2022-03-20 22:03:42 +1100

    update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('#view-work').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
